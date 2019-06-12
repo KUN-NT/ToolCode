4fc8e3031168b85648c7561600ef029223eada2c
KUN-NT <332271028@qq.com>
2019-06-12 08:41:03 +0800

    first commit

---
+++ 后台/MD5Helper.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaterComman
+{
+    /// <summary>
+    /// 生成MD5值
+    /// </summary>
+    public partial class MD5Helper
+    {
+        public static string EncryptString(string str)
+        {
+            //创建对象的方法：构造方法、静态方法(工厂)
+            MD5 md5 = MD5.Create();
+            byte[] byteOld = Encoding.UTF8.GetBytes(str);
+            byte[] byteNew = md5.ComputeHash(byteOld);
+            StringBuilder build=new StringBuilder();
+            foreach(byte b in byteNew)
+            {
+                //减肥字符转换称16进制表示的字符串、而且是恒占用两位
+                build.Append(b.ToString("x2"));
+            }
+            return build.ToString();
+        }
+    }
+}
---
+++ 后台/PinYin/PimYinhelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.International.Converters.PinYinConverter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace CaterComman
+{
+    /// <summary>
+    /// 生成拼音
+    /// </summary>
+    public partial class PimYinhelper
+    {
+        public static string GetPinYin(string str)
+        {
+            string s = "";
+            foreach (char cc in str)
+            {
+                ChineseChar ch = new ChineseChar(cc);
+                s += ch.Pinyins[0][0];
+            }
+            return s;
+        }
+    }
+}
